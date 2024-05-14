a4d05271b3fde6571aaa7e9d0236147081b059ab
Blas Hernández <blashernandez@ibm.com>
2024-05-13 22:45:03 -0300

    dockerfile and requirements added

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app.include_router(client_router.router)
 app.include_router(contract_router.router)
 app.include_router(payment_router.router)
 
+@app.get("/")
+def index():
+    return app.openapi()
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
